7ad236c1d293a7a89ebf6934d395b5dfa2aad7f1
ThangNS32 <hanhlinhdisney2005@gmail.com>
2025-09-02 00:55:12 +0700

    Add Practical Work 4

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -10,6 +10,10 @@ import android.view.ViewGroup;
 
 import android.graphics.Color;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 
@@ -20,8 +24,22 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState){
-        //Tạo một View rỗng
-        FrameLayout layout = new FrameLayout(requireContext());
+        //Tạo LinearLayout theo chiều dọc
+        LinearLayout layout = new LinearLayout(getActivity());
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        //Thêm TextView hiển thị ngày
+        TextView dayText = new TextView(getActivity());
+        dayText.setText("Thursday");
+        dayText.setTextSize(24);
+        dayText.setPadding(20,20,20,20);
+
+        //Thêm ImageView hiển thị icon thời tiết
+        ImageView weatherIcon = new ImageView(getActivity());
+        weatherIcon.setImageResource(R.drawable.sunny);
+
+        layout.addView(dayText);
+        layout.addView(weatherIcon);
 
         // Đặt màu nền
         layout.setBackgroundColor(Color.parseColor("#20FF0000"));
