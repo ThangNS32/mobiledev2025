3065a73e84a91d2f23aa5675d733158cfc3a9084
ThangNS32 <hanhlinhdisney2005@gmail.com>
2025-09-05 10:52:14 +0700

    Add Practical Work 5

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -19,31 +19,62 @@ import androidx.annotation.NonNull;
 
 public class ForecastFragment extends Fragment {
 
+    private String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    private String[] infos = {
+            "Cloudy 24°C - 31°C",
+            "Heavy Rain 25°C - 32°C",
+            "Rain 22°C - 23°C",
+            "Snow 23°C - 24°C",
+            "Storm 24°C - 25°C",
+            "Sunny 25°C - 26°C",
+            "Windy 26°C - 27°C"
+    };
+    private int[] icons = {
+            R.drawable.cloud,
+            R.drawable.rain,
+            R.drawable.heavy_rain,
+            R.drawable.snow,
+            R.drawable.storm,
+            R.drawable.sunny,
+            R.drawable.windy
+    };
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState){
-        //Tạo LinearLayout theo chiều dọc
-        LinearLayout layout = new LinearLayout(getActivity());
-        layout.setOrientation(LinearLayout.VERTICAL);
+       View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
 
-        //Thêm TextView hiển thị ngày
-        TextView dayText = new TextView(getActivity());
-        dayText.setText("Thursday");
-        dayText.setTextSize(24);
-        dayText.setPadding(20,20,20,20);
+       LinearLayout forcastContainer = rootView.findViewById(R.id.forecast_container);
 
-        //Thêm ImageView hiển thị icon thời tiết
-        ImageView weatherIcon = new ImageView(getActivity());
-        weatherIcon.setImageResource(R.drawable.sunny);
+       // Tạo 7 dòng dự báo
+       for (int i = 0; i < days.length; i++) {
+           // Layout ngang cho mỗi dòng
+           LinearLayout row = new LinearLayout(getContext());
+           row.setOrientation(LinearLayout.HORIZONTAL);
+           row.setPadding(8, 16, 8,16);
 
-        layout.addView(dayText);
-        layout.addView(weatherIcon);
+           TextView dayText = new TextView(getContext());
+           dayText.setText(days[i]);
+           dayText.setTextSize(18f);
+           dayText.setPadding(0, 0, 24, 0);
 
-        // Đặt màu nền
-        layout.setBackgroundColor(Color.parseColor("#20FF0000"));
+           ImageView weatherIcon = new ImageView(getContext());
+           weatherIcon.setImageResource(icons[i]);
+           weatherIcon.setPadding(0, 0, 24, 0);
 
-        return layout;
+           TextView infoText = new TextView(getContext());
+           infoText.setText(infos[i]);
+           infoText.setTextSize(16f);
+           infoText.setPadding(24,0,0,0);
+
+           row.addView(dayText);
+           row.addView(weatherIcon);
+           row.addView(infoText);
+
+           forcastContainer.addView(row);
+       }
+
+        return rootView;
     }
 }
